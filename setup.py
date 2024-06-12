405f46fd37aa2bf5aef7f9954786b3a8800d3594
Olivia Schleifer <oschleifer@g.hmc.edu>
2024-06-12 10:53:30 -0700

    ../nanocomp entry point

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     include_package_data=True,
     entry_points={
         "console_scripts": [
-            "NanoComp=nanocomp.NanoComp:main",
+            "NanoComp=../nanocomp.NanoComp:main",
         ],
     },
 )
